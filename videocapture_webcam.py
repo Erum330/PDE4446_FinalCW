058ec37c0c85a434c0617a93e26aeb45b49994f6
Erum330 <58556663+Erum330@users.noreply.github.com>
2025-11-16 17:33:05 +0400

    BGR to HSV

--- videocapture_webcam.py
+++ videocapture_webcam.py
@@ -6,6 +6,10 @@ while True:
     isTrue, frame = capture.read()
     cv.imshow("Video", frame)
 
+    hsv_frame=cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    cv.imshow("HSV_Video", hsv_frame)
+
+
     if cv.waitKey(20) & 0xFF == ord("d"):
         break
 
